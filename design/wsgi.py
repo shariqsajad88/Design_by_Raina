c352d30d4841a08d2f2f55c3df24654e414b5cc7
shariqsajad88 <bhatshariq88@gmail.com>
2025-04-17 16:20:11 +0530

    Fix static files for Railway using WhiteNoise

--- design/wsgi.py
+++ design/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'design.settings')
 
 application = get_wsgi_application()
+
+web: gunicorn design.wsgi --bind 0.0.0.0:$PORT
