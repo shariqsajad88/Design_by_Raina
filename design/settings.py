c9d3eb25b065ee52f0eb851fe1c65dcbccb3ddb9
shariqsajad88 <bhatshariq88@gmail.com>
2025-04-22 12:39:54 +0530

    added smtp

--- design/settings.py
+++ design/settings.py
@@ -134,3 +134,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rainasaman0001@gmail.com'
+EMAIL_HOST_PASSWORD = 'vqnm dhli rfmp iiex'  # NOT your Gmail password
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CONTACT_EMAIL = 'rainasaman0001@gmail.com'  # Email to receive the form
+
